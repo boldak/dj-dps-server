7584dca47a64ed64c4dd2191da1ce545e7195369
boldak <boldak.andrey@gmail.com>
2017-12-15 15:57:29 +0200

    accept client from req

--- api/controllers/DPSController.js
+++ api/controllers/DPSController.js
@@ -12,14 +12,14 @@ module.exports = {
     run: function(req, resp) {
 
         var script = req.body.script;
-        
         var state = req.body.state;
-        
-      
         var locale = req.body.locale || "en";
         locale = (locale == "uk") ? "ua" : locale;
 
-        state = (state) || {locale : locale}
+        state = (state) || {
+            locale : locale, 
+            client: req.body.client
+        }
         // console.log(req.host, script, state)
         var executable = new Script()
             .host(req.host)
